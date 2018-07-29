f723b1820150d9c2584b3257b79ab46626a0e1e0
Ian McWilliam <kaosagnt@gmail.com>
2018-07-29 11:22:38 +1000

    playbooks -> rhel -> security -> Update the RHEL scripts from RedHat to their latest versions.
    cve-2017-1000253-1.sh
    cve-2017-6074-2.sh
    cve-2018-3639--2018-07-20-1546.sh
    spectre-meltdown--2018-07-20-1546.sh

--- playbooks/rhel/security/scripts/spectre-meltdown--2018-07-20-1546.sh
+++ playbooks/rhel/security/scripts/spectre-meltdown--2018-07-20-1546.sh
@@ -7,7 +7,7 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# Version: 2.4
+VERSION="2.8"
 
 # Warning! Be sure to download the latest version of this script from its primary source:
 # https://access.redhat.com/security/vulnerabilities/speculativeexecution
@@ -39,11 +39,11 @@ basic_args() {
 
     local parameters=( "$@" )
 
-    RED="\033[1;31m"
-    YELLOW="\033[1;33m"
-    GREEN="\033[1;32m"
-    BOLD="\033[1m"
-    RESET="\033[0m"
+    RED="\\033[1;31m"
+    YELLOW="\\033[1;33m"
+    GREEN="\\033[1;32m"
+    BOLD="\\033[1m"
+    RESET="\\033[0m"
     for parameter in "${parameters[@]}"; do
         if [[ "$parameter" == "-h" || "$parameter" == "--help" ]]; then
             echo "Usage: $( basename "$0" ) [-n | --no-colors] [-d | --debug]"
@@ -74,6 +74,7 @@ basic_reqs() {
 
     # Disclaimer
     echo
+    echo -e "Spectre/Meltdown Detection Script Ver. $VERSION"
     echo -e "${BOLD}This script is primarily designed to detect $CVE on supported"
     echo -e "Red Hat Enterprise Linux systems and kernel packages."
     echo -e "Result may be inaccurate for other RPM based systems.${RESET}"
@@ -117,16 +118,17 @@ get_rhel() {
 
     local running_kernel="$1"
 
-    local rhel=$( sed -r -n 's/^.*el([[:digit:]]).*$/\1/p' <<< "$running_kernel" )
+    local rhel
+    rhel=$( sed -r -n 's/^.*el([[:digit:]]).*$/\1/p' <<< "$running_kernel" )
     echo "$rhel"
 }
 
 
 check_cpu_vendor() {
-    # Checks for supported CPU vendor.
+    # Checks for supported CPU vendor/model/architecture.
     #
     # Prints:
-    #     'Intel' or 'AMD'
+    #     'Intel', 'AMD', 'POWER'
     #
     # Returns:
     #     0 if supported CPU vendor found, otherwise 1
@@ -144,6 +146,10 @@ check_cpu_vendor() {
         echo "AMD"
         return 0
     fi
+    if grep --quiet "POWER" "$cpuinfo"; then
+        echo "POWER"
+        return 0
+    fi
 
     return 1
 }
@@ -159,16 +165,50 @@ gather_info() {
     #     MOCK_DEBUG_X86_PATH can be used to mock /sys/kernel/debug/x86 directory
     #     MOCK_VULNS_PATH can be used to mock /sys/devices/system/cpu/vulnerabilities directory
     #     MOCK_CMDLINE_PATH can be used to mock /proc/cmdline file
-    #     MOCK_EUID can be used to mock EUID variable
+    #     MOCK_LOG_DMESG_PATH can be used to mock /var/log/dmesg
 
     local debug_x86=${MOCK_DEBUG_X86_PATH:-/sys/kernel/debug/x86}
     local vulns=${MOCK_VULNS_PATH:-/sys/devices/system/cpu/vulnerabilities}
     local cmdline_path=${MOCK_CMDLINE_PATH:-/proc/cmdline}
-    local euid=${MOCK_EUID:-$EUID}
+    local dmesg_log_path=${MOCK_LOG_DMESG_PATH:-/var/log/dmesg}
 
-    # Am I root?
-    if (( euid == 0 )); then
-        root=1
+    # Are cpu/vulnerabilities files present?
+    if [[ -r "${vulns}/meltdown" && -r "${vulns}/spectre_v1" && -r "${vulns}/spectre_v2" ]]; then
+        all_vuln_files=1
+        retpo_kernel=1
+    fi
+
+    # Read status from vulnerabilities files
+    if (( all_vuln_files )); then
+        if ! grep --quiet 'Vulnerable' "${vulns}/meltdown"; then
+            vulns_md_mitigation=1
+        fi
+        if ! grep --quiet 'Vulnerable' "${vulns}/spectre_v1"; then
+            vulns_sv1_mitigation=1
+        fi
+        if ! grep --quiet 'Vulnerable' "${vulns}/spectre_v2"; then
+            vulns_sv2_mitigation=1
+        fi
+    fi
+
+    # Read commandline
+    if grep --quiet 'nopti' "$cmdline_path"; then
+        nopti=1
+    fi
+    if grep --quiet 'noibrs' "$cmdline_path"; then
+        noibrs=1
+    fi
+    if grep --quiet 'noibpb' "$cmdline_path"; then
+        noibpb=1
+    fi
+    if grep --quiet 'no_rfi_flush' "$cmdline_path"; then
+        norfi=1
+    fi
+    if grep --quiet 'nospectre_v2' "$cmdline_path"; then
+        nospectre_v2=1
+    fi
+    if grep --quiet '[[:space:]]spectre_v2=' "$cmdline_path"; then
+        spectre_v2=$( sed -r 's/.*[[:space:]]spectre_v2=([a-zA-Z]+).*/\1/' "$cmdline_path" )
     fi
 
     # Is debugfs mounted?
@@ -177,14 +217,8 @@ gather_info() {
     fi
 
     # Will fallback detection be needed?
-    if (( ! mounted_debugfs || ! root )); then
+    if (( ! mounted_debugfs )); then
         fallback_needed=1
-    fi
-
-    # Are cpu/vulnerabilities files present?
-    if [[ -r "${vulns}/meltdown" && -r "${vulns}/spectre_v1" && -r "${vulns}/spectre_v2" ]]; then
-        all_vuln_files=1
-        retpo_kernel=1
     fi
 
     # Are all debug files accessible?
@@ -197,19 +231,6 @@ gather_info() {
             all_debug_files=1
         fi
     fi
-
-    # Read status from vulnerabilities files
-    if (( all_vuln_files )); then
-        if grep --quiet -v 'Vulnerable' ${vulns}/meltdown; then
-            vulns_md_mitigation=1
-        fi
-        if grep --quiet -v 'Vulnerable' ${vulns}/spectre_v1; then
-            vulns_sv1_mitigation=1
-        fi
-        if grep --quiet -v 'Vulnerable' ${vulns}/spectre_v2; then
-            vulns_sv2_mitigation=1
-        fi
-    fi  
 
     # Read features from debugfs
     if (( all_debug_files )); then
@@ -224,23 +245,30 @@ gather_info() {
         fi
     fi
 
-    # Read features from dmesg
-    if ! dmesg | grep --quiet 'Linux.version'; then
-        dmesg_wrapped=1
+    # Read features from dmesg, use log file first, fallback to circular buffer
+    if [[ -r "$dmesg_log_path" ]]; then
+        dmesg_data=$( <"$dmesg_log_path" )
+        dmesg_log_used=1
+    else
+        dmesg_data=$( dmesg )
+        dmesg_command_used=1
+        if ! grep --quiet 'Linux.version' <<< "$dmesg_data"; then
+            dmesg_wrapped=1
+        fi
     fi
 
     # These will not appear if disabled from commandline
-    if dmesg | grep --quiet -e 'x86/pti: Unmapping kernel while in userspace' \
-                            -e 'x86/pti: Kernel page table isolation enabled' \
-                            -e 'x86/pti: Xen PV detected, disabling' \
-                            -e 'x86/pti: Xen PV detected, disabling PTI protection' \
-                            -e 'Kernel page table isolation enabled'; then
+    if grep --quiet -e 'x86/pti: Unmapping kernel while in userspace' \
+                    -e 'x86/pti: Kernel page table isolation enabled' \
+                    -e 'x86/pti: Xen PV detected, disabling' \
+                    -e 'x86/pti: Xen PV detected, disabling PTI protection' \
+                    -e 'Kernel page table isolation enabled' <<< "$dmesg_data"; then
         new_kernel=1
         pti_dmesg=1
     fi
 
     # These will appear if disabled from commandline
-    line=$( dmesg | grep 'FEATURE SPEC_CTRL' | tail -n 1 )  # Check last
+    line=$( grep 'FEATURE SPEC_CTRL' <<< "$dmesg_data" | tail -n 1 )  # Check last
     if [[ "$line" ]]; then
         new_kernel=1
         if ! grep --quiet 'Not Present' <<< "$line"; then
@@ -251,7 +279,7 @@ gather_info() {
         fi
     fi
 
-    line=$( dmesg | grep 'FEATURE IBPB_SUPPORT' | tail -n 1 )   # Check last
+    line=$( grep 'FEATURE IBPB_SUPPORT' <<< "$dmesg_data" | tail -n 1 )   # Check last
     if [[ "$line" ]]; then
         new_kernel=1
         if ! grep --quiet 'Not Present' <<< "$line"; then
@@ -261,25 +289,6 @@ gather_info() {
             not_ibpb_dmesg=1
         fi
     fi
-
-    # Read commandline
-    if grep --quiet 'nopti' "$cmdline_path"; then
-        nopti=1
-    fi
-    if grep --quiet 'noibrs' "$cmdline_path"; then
-        noibrs=1
-    fi
-    if grep --quiet 'noibpb' "$cmdline_path"; then
-        noibpb=1
-    fi
-    if grep --quiet 'nospectre_v2' "$cmdline_path"; then
-        nospectre_v2=1
-    fi
-
-    if grep --quiet -e '\sspectre_v2=' "$cmdline_path"; then
-        spectre_v2=$(sed "s/.*\sspectre_v2=\([a-zA-Z]\)/\1/" $cmdline_path)
-    fi
-
 }
 
 
@@ -289,7 +298,7 @@ check_variants() {
     # Side effects:
     #     Sets global variable `result`, a bitmask of vulnerable variants
 
-    # If vulnerabilities files are available, rely on them 
+    # If vulnerabilities files are available, rely on them
     if (( all_vuln_files )); then
         if (( vulns_sv1_mitigation )); then
             (( result &= ~2 ))
@@ -339,7 +348,7 @@ check_variants() {
                 (( result &= ~4 )) # Pre-retpoline kernels on old CPUs which don't need microcode
             fi
         else
-            if (( ibpb_dmesg && ! noibpb && && ! noibrs && ! nospectre_v2 )); then
+            if (( ibpb_dmesg && ! noibpb && ! noibrs && ! nospectre_v2 )); then
                 (( result &= ~4 )) # Fallback detection -- assume kernel-set defaults based on dmesg
             fi
         fi
@@ -348,7 +357,7 @@ check_variants() {
 
 
 get_results() {
-  # Set messsages to display vulnerability/mitigation status
+  # Set messages to display vulnerability/mitigation status
   # to the user.
   # Sets the following globals:
   # pti, ibrs, ibpb - Whether these features were disabled by the user
@@ -358,7 +367,7 @@ get_results() {
 
     local vulns=${MOCK_VULNS_PATH:-/sys/devices/system/cpu/vulnerabilities}
 
-    if (( new_kernel )); then
+    if (( new_kernel || retpo_kernel )); then
         kernel_with_patches="${GREEN}OK${RESET}"
         if (( hw_support )); then
             hw="${GREEN}YES${RESET}"
@@ -402,6 +411,18 @@ get_results() {
         retpolines="Not disabled on kernel commandline"
     fi
 
+    if (( nopti || norfi )); then
+        if (( ! norfi )); then
+            rfiflush="${RED}Disabled on kernel commandline by 'nopti'${RESET}"
+        elif (( ! nopti )); then
+            rfiflush="${RED}Disabled on kernel commandline by 'no_rfi_flush'${RESET}"
+        else
+            rfiflush="${RED}Disabled on kernel commandline by 'nopti' and 'no_rfi_flush'${RESET}"
+        fi
+    else
+        rfiflush="Not disabled on kernel commandline"
+    fi
+
     if (( all_vuln_files )); then
         variant_1=$( <"${vulns}/spectre_v1" )
         variant_2=$( <"${vulns}/spectre_v2" )
@@ -438,7 +459,47 @@ get_results() {
 }
 
 
+get_virtualization() {
+    # Gets virtualization type.
+    #
+    # Prints:
+    #     Virtualization type, "None", or "virt-what not available"
+
+    local virt
+    if command -v virt-what &> /dev/null; then
+        virt=$( virt-what 2>&1 | tr '\n' ' ' )
+        if [[ "$virt" ]]; then
+            echo "$virt"
+        else
+            echo "None"
+        fi
+    else
+        echo "virt-what not available"
+    fi
+}
+
+
+require_root() {
+    # Checks if user is root.
+    #
+    # Side effects:
+    #     Exits when user is not root.
+    #
+    # Notes:
+    #     MOCK_EUID can be used to mock EUID variable
+
+    local euid=${MOCK_EUID:-$EUID}
+
+    # Am I root?
+    if (( euid != 0 )); then
+        echo "This script must run with elevated privileges (e.g. as root)"
+        exit 1
+    fi
+}
+
+
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    require_root  # Needed for virt-what and reading debugfs
     basic_args "$@"
     basic_reqs "Spectre / Meltdown"
     running_kernel=$( uname -r )
@@ -451,12 +512,14 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
 
     vendor=$( check_cpu_vendor )
     if (( $? == 1 )); then
-        echo "Your CPU vendor is not supported by the script at the moment."
-        echo "Only Intel and AMD are supported for now."
-        exit 1
+        # Archictectures other than x86_64, x86, POWER
+        # are supported on a best-effort basis if vuln
+        # files are present
+        unspecified_arch=1
+    else
+        unspecified_arch=0
     fi
 
-    root=0
     mounted_debugfs=0
     all_debug_files=0
     fallback_needed=0
@@ -478,6 +541,9 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
     nospectre_v2=0
     spectre_v2="auto"
     hw_support=0
+    dmesg_log_used=0
+    dmesg_command_used=0
+    virtualization=""
 
     # /sys/devices/system/cpu/vulnerabilities updates
     # See comments on line 23 above
@@ -496,13 +562,13 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
     gather_info
     check_variants
     get_results
+    virtualization=$( get_virtualization )
 
     # Debug prints
     if [[ "$debug" ]]; then
         echo "running_kernel = *$running_kernel*"
         echo "rhel = *$rhel*"
         echo "vendor = *$vendor*"
-        echo "root = *$root*"
         echo "mounted_debugfs = *$mounted_debugfs*"
         echo "all_debug_files = *$all_debug_files*"
         echo "fallback_needed = *$fallback_needed*"
@@ -521,6 +587,7 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
         echo "nopti = *$nopti*"
         echo "noibrs = *$noibrs*"
         echo "noibpb = *$noibpb*"
+        echo "norfi = *$norfi*"
         echo "hw_support = *$hw_support*"
         echo "variant_1 = *$variant_1*"
         echo "variant_2 = *$variant_2*"
@@ -532,40 +599,63 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
         echo "nospectre_v2 = *$nospectre_v2*"
         echo "spectre_v2 = *$spectre_v2*"
         echo "retpolines = *$retpolines*"
+        echo "unspecified_arch = *$unspecified_arch*"
+        echo "dmesg_log_used = *$dmesg_log_used*"
+        echo "dmesg_command_used = *$dmesg_command_used*"
         echo "result = *$result*"
+        echo "virtualization = *$virtualization*"
         echo
     fi
 
     # Output
     echo -e "Detected CPU vendor: ${BOLD}$vendor${RESET}"
     echo -e "Running kernel: ${BOLD}$running_kernel${RESET}"
+    echo -e "Virtualization: ${BOLD}$virtualization${RESET}"
     echo
 
+    if (( ! all_vuln_files )); then
+        if [[ "$unspecified_arch" == 1 || "$vendor" == "POWER" ]]; then
+            echo "This system's kernel does not provide detailed vulnerability information."
+            echo "Fallback detection for this CPU vendor is not supported by the script at the moment."
+            echo "Only Intel/AMD x86/x86_64 and IBM POWER (RHEL 7) are supported for now."
+            exit 1
+        else
+            echo "Detailed information about this CPU architecture may not be available."
+            echo "Presently, only Intel/AMD x86/x86_64 and IBM POWER are fully supported."
+        fi
+    fi
+
     # Warnings
-    if (( dmesg_wrapped )); then
-        echo -e "${YELLOW}It seems that dmesg circular buffer already wrapped,${RESET}"
-        echo -e "${YELLOW}the results may be inaccurate.${RESET}"
-        echo
+    if [[ "$vendor" == "AMD" || "$vendor" == "Intel" ]]; then
+        if (( dmesg_wrapped )); then
+            echo -e "${YELLOW}It seems that dmesg circular buffer already wrapped,${RESET}"
+            echo -e "${YELLOW}the results may be inaccurate.${RESET}"
+            echo
+        fi
     fi
 
     # Variants
     echo -e "Variant #1 (Spectre): $variant_1"
     echo -e "CVE-2017-5753 - speculative execution bounds-check bypass"
-    echo -e "   - Kernel with mitigation patches: $kernel_with_patches"
+    if [[ ( "$vendor" == "AMD" ) || ( "$vendor" == "Intel" ) ]]; then
+        echo -e "   - Kernel with mitigation patches: $kernel_with_patches"
+    fi
     echo
 
     echo -e "Variant #2 (Spectre): $variant_2"
     echo -e "CVE-2017-5715 - speculative execution branch target injection"
-    if (( rhel == 5 )); then
-        echo -e "   - Kernel with mitigation patches: ${RED}NO${RESET}"
-        echo -e "   - HW support / updated microcode: ${YELLOW}Cannot detect without updated kernel${RESET}"
-    else
-        echo -e "   - Kernel with mitigation patches: $kernel_with_patches"
-        echo -e "   - HW support / updated microcode: $hw"
+    if [[ ( "$vendor" == "AMD" ) || ( "$vendor" == "Intel" ) ]]; then
+        if (( rhel == 5 && ! retpo_kernel )); then
+            echo -e "   - Kernel with mitigation patches: ${RED}NO${RESET}"
+            echo -e "   - HW support / updated microcode: ${YELLOW}Cannot detect without updated kernel${RESET}"
+        else
+            echo -e "   - Kernel with mitigation patches: $kernel_with_patches"
+            echo -e "   - HW support / updated microcode: $hw"
+        fi
+        echo -e "   - IBRS: $ibrs"
+        echo -e "   - IBPB: $ibpb"
+        echo -e "   - Retpolines: $retpolines"
     fi
-    echo -e "   - IBRS: $ibrs"
-    echo -e "   - IBPB: $ibpb"
-    echo -e "   - Retpolines: $retpolines"
     echo
 
     echo -e "Variant #3 (Meltdown): $variant_3"
@@ -574,20 +664,33 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
         echo -e "   - AMD CPU: ${GREEN}OK${RESET}"
     else
         echo -e "   - Kernel with mitigation patches: $kernel_with_patches"
-        echo -e "   - PTI: $pti"
+        if [[ "$vendor" == "Intel" ]]; then
+            echo -e "   - PTI: $pti"
+        fi
+        if [[ "$vendor" == "POWER" ]]; then
+            echo -e "   - RFI Flush: $rfiflush"
+        fi
     fi
     echo
 
     if (( result != 0 )); then
+        if (( unspecified_arch )); then
+            echo "Detailed remediation steps may not be available."
+        fi
         echo "Red Hat recommends that you:"
         if (( ! retpo_kernel )); then
             echo -e "* Update your kernel and reboot the system."
         fi
-        if (( ! hw_support )); then
-            echo -e "* Ask your HW vendor for CPU microcode update."
+        if [[ "$vendor" == "AMD" || "$vendor" == "Intel" ]]; then
+            if (( ! hw_support )); then
+                echo -e "* Ask your HW vendor for CPU microcode update."
+            fi
         fi
-        if (( noibrs || noibpb || nopti || nospectre_v2 )); then
+        if (( noibrs || noibpb || nopti || norfi || nospectre_v2 )); then
             echo -e "* Remove kernel commandline options as noted above."
+        fi
+        if [[ $variant_2 == "${RED}Vulnerable: Retpoline with unsafe module(s)${RESET}" ]]; then
+            echo -e "* See https://access.redhat.com/solutions/3399691 to determine which modules are vulnerable."
         fi
         echo
     fi
@@ -606,21 +709,20 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
                 echo "    # systemctl restart sys-kernel-debug.mount"
             fi
         fi
-        if (( ! root )); then
-            echo "* Run this script with elevated privileges (e.g. as root)"
-        fi
         echo
     fi
 
-    echo -e "${BOLD}Note about virtualization${RESET}"
-    echo -e "In virtualized environment, there are more steps to mitigate the issue, including:"
-    echo -e "* Host needs to have updated kernel and CPU microcode"
-    echo -e "* Host needs to have updated virtualization software"
-    echo -e "* Guest needs to have updated kernel"
-    echo -e "* Hypervisor needs to propagate new CPU features correctly"
-    echo -e "For more details about mitigations in virtualized environment see:"
-    echo -e "https://access.redhat.com/articles/3331571"
-    echo
+    if [[ "$virtualization" != "None" ]]; then
+        echo -e "${BOLD}Note about virtualization${RESET}"
+        echo -e "In virtualized environment, there are more steps to mitigate the issue, including:"
+        echo -e "* Host needs to have updated kernel and CPU microcode"
+        echo -e "* Host needs to have updated virtualization software"
+        echo -e "* Guest needs to have updated kernel"
+        echo -e "* Hypervisor needs to propagate new CPU features correctly"
+        echo -e "For more details about mitigations in virtualized environment see:"
+        echo -e "https://access.redhat.com/articles/3331571"
+        echo
+    fi
 
     echo -e "For more information about the vulnerabilities see:"
     echo -e "https://access.redhat.com/security/vulnerabilities/speculativeexecution"
